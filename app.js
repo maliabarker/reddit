a296a9d24527927b57afba9cc43d714f431882da
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-10 15:37:44 -1000

    started layout of express and handlebars project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
