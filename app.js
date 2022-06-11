5f9a753685c4b17d58de9f1cbb2c9661dbcdfc14
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-10 16:27:18 -1000

    Users can submit and save posts

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set db
+require('./data/reddit-db');
+
+const posts = require('./controllers/posts')(app);
+
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+// NEW POST
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+})
 
 app.listen(3000);
