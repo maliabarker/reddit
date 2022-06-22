da1c430e0b0c22529803b51b3079c380646033ef
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-22 13:44:24 -1000

    added a authentication check for replies

--- controllers/replies.js
+++ controllers/replies.js
@@ -23,31 +23,36 @@ module.exports = (app) => {
     });
 
     // CREATE REPLY
-app.post('/posts/:postId/comments/:commentId/replies', (req, res) => {
-    // TURN REPLY INTO A COMMENT OBJECT
-    const reply = new Comment(req.body);
-    reply.author = req.user._id;
-    reply.post = req.params.postId;
-    // LOOKUP THE PARENT POST
-    Post.findById(req.params.postId)
-        .then((post) => {
-            // FIND THE CHILD COMMENT
-            Promise.all([
-                reply.save(),
-                Comment.findById(req.params.commentId),
-            ])
-            .then(([reply, comment]) => {
-                // ADD THE REPLY
-                comment.comments.unshift(reply._id);
-                return Promise.all([
-                    comment.save(),
-                ]);
-            })
-            .then(() => res.redirect(`/posts/${req.params.postId}`))
-            .catch(console.error);
-        // SAVE THE CHANGE TO THE PARENT DOCUMENT
-        return post.save();
-      });
-  });
+    app.post('/posts/:postId/comments/:commentId/replies', (req, res) => {
+        if (req.user) {
+            // TURN REPLY INTO A COMMENT OBJECT
+            const reply = new Comment(req.body);
+            reply.author = req.user._id;
+            reply.post = req.params.postId;
+            // LOOKUP THE PARENT POST
+            Post.findById(req.params.postId)
+                .then((post) => {
+                    // FIND THE CHILD COMMENT
+                    Promise.all([
+                        reply.save(),
+                        Comment.findById(req.params.commentId),
+                    ])
+                    .then(([reply, comment]) => {
+                        // ADD THE REPLY
+                        comment.comments.unshift(reply._id);
+                        return Promise.all([
+                            comment.save(),
+                        ]);
+                    })
+                    .then(() => res.redirect(`/posts/${req.params.postId}`))
+                    .catch(console.error);
+                // SAVE THE CHANGE TO THE PARENT DOCUMENT
+                return post.save();
+            });
+        } else {
+            res.render('error', { errorMessage:'You need to be logged in to reply to comments!' })
+            return res.status(401); // UNAUTHORIZED
+        };
+    });
 
 };
